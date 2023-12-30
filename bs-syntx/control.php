7bd3f51da3fb5e8e45c895fc0cac3a9f8770507f
Lenka Florienka <lenkamelinda7@gmail.com>
2023-12-30 15:55:17 +0700

    modify control.php

--- bs-syntx/control.php
+++ bs-syntx/control.php
@@ -31,13 +31,13 @@ do {
 </head>
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
-        <?php for ($i = 1; $i <= 3; $i++) {?>
+        <?php for ($i = 1; $i <= 3; $i++) : ?>
             <tr>
-                <?php for ($j = 1; $j <= 5; $j ++){ ?>
-                    <td> <?php echo "$i,$j" ?></td>
-                    <?php } ?>
+                <?php for ($j = 1; $j <= 5; $j ++) : ?>
+                    <td> <?= "$i, $j" ?></td>
+                    <?php endfor; ?>
             </tr>
-            <?php } ?>
+            <?php endfor; ?>
     </table>
 </body>
 </html>
