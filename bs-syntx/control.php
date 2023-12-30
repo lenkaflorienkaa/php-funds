3d38ac4e568f5414ff82e8c86f7aca85eca86f76
Lenka Florienka <lenkamelinda7@gmail.com>
2023-12-30 15:50:26 +0700

    modify looping practice - template

--- bs-syntx/control.php
+++ bs-syntx/control.php
@@ -27,19 +27,17 @@ do {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Practice 1</title>
+    <title>Practice 2</title>
 </head>
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
-        <?php 
-            for ($i = 1; $i <= 3; $i++){
-                echo "<tr>";
-                for ( $j = 1; $j <= 5; $j++){
-                    echo "<td>$i,$j</td>";
-                }
-                echo "</tr>";
-            }
-        ?>
+        <?php for ($i = 1; $i <= 3; $i++) {?>
+            <tr>
+                <?php for ($j = 1; $j <= 5; $j ++){ ?>
+                    <td> <?php echo "$i,$j" ?></td>
+                    <?php } ?>
+            </tr>
+            <?php } ?>
     </table>
 </body>
 </html>
